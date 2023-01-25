6d993aada513c48621f6b39323f368e9b737460e
Eryk <eryk658@wp.pl>
2023-01-25 20:07:16 +0100

    add code comments, address for otodom and code refactor

--- main_nieruchomosci_online.py
+++ main_nieruchomosci_online.py
@@ -14,22 +14,23 @@ if __name__ == "__main__":
 
     driver = webdriver.Chrome(chrome_options=options)
 
-    listing_page_url = "https://www.nieruchomosci-online.pl/szukaj.html?3,mieszkanie,sprzedaz,,Kraków:5600"
-    file_name = "nieruchomosci_online_krakow.csv"
+    # take number of listing pages regarding specified city (first listing page)
+    listing_page_url = "https://warszawa.nieruchomosci-online.pl"
+    file_name = "nieruchomosci/nieruchomosci_online_warszawa6.csv"
+
     driver.get(listing_page_url)
     source_code = driver.page_source
     bs = BeautifulSoup(source_code, "html.parser")
 
-    # take number of listing pages regarding specified city (first listing page)
     pagination_wrapper = bs.find("div", attrs={"id": "pagination-outer"})
     pagination_list = pagination_wrapper.find("ul", attrs={"class": "pagination-mob-sub"})
     pagination_list_element = pagination_list.contents[-2]
     list_site_max_number = int(pagination_list_element.text)
 
-    paths = []
     # Take ad urls from the listing page
-    for i in range(1, list_site_max_number + 1):
-        driver.get(f"{listing_page_url}?p={i}")
+    paths = []
+    for i in range(400, 500):
+        driver.get(f"{listing_page_url}/?p={i}")
         source_code = driver.page_source
         bs = BeautifulSoup(source_code, "html.parser")
 
@@ -42,8 +43,6 @@ if __name__ == "__main__":
                     a_href = a_tag.attrs["href"]
                     paths.append(a_href)
 
-    print(paths)
-    print(len(paths))
     # paths = [
     #     "https://warszawa.nieruchomosci-online.pl/mieszkanie-w-apartamentowcu,wysoki-standard/23877359.html",
     #     "https://warszawa.nieruchomosci-online.pl/mieszkanie,z-aneksem-kuchennym/23787685.html"
@@ -57,82 +56,88 @@ if __name__ == "__main__":
 
         # Take the most important information about advertisement like price and area
         price_main_div = bs.find("div", attrs={"id": "bottomBox"})
-        primary_price_element = price_main_div.find("p", attrs={"class": "info-primary-price"})
-        area_element = price_main_div.find("p", attrs={"class": "info-area"})
-        secondary_price_element = price_main_div.find("p", attrs={"class": "info-secondary-price"})
-        address_element = price_main_div.find("h1")
+        if price_main_div:
+            primary_price_element = price_main_div.find("p", attrs={"class": "info-primary-price"})
+            area_element = price_main_div.find("p", attrs={"class": "info-area"})
+            secondary_price_element = price_main_div.find("p", attrs={"class": "info-secondary-price"})
+            address_element = price_main_div.find("h1")
 
-        # Check if ad contain price. Some advertisements don't contain price because they require
-        # contact with the saler
-        if primary_price_element and secondary_price_element:
-            primary_price = unidecode(primary_price_element.text)
-            secondary_price = unidecode(secondary_price_element.text)
-        else:
-            # Price not included in the advertisement
-            primary_price = "Zapytaj o cene"
-            secondary_price = "Zapytaj o cene"
-        area = unidecode(area_element.text)
-        address = unidecode(address_element.text)
-
-        res = {
-            "URL": path,
-            "Cena": primary_price,
-            "Powierzchnia": area,
-            "Cena za metr": secondary_price,
-            "Adres": address,
-        }
-
-        # take information under the photo, like floor and year of construction
-        main_offer_div = bs.find("div", attrs={"class": "box-offer-inside mod-a mobile"})
-        main_info_div = main_offer_div.find("div", attrs={"class": "table-d__changer"})
-        main_info_divs = main_info_div.contents
-        for info_div in main_info_divs:
-            # iterate over every element with main information (under photo)
-            if info_div.name == "div":
-                info = info_div.contents[1]
-                title_element = info.find("p")
-                value_elements = info.find_all("span")
-
-                title = title_element.text
-                title = unidecode(title)
-
-                # value of main info can contain multiple span elements e.g. floor 3/3.
-                # Need to iterate over all elements to create value string
-                value = ""
-                for value_element in value_elements:
-                    value += value_element.text
-                value = unidecode(value)
-
-                res[title] = value
-
-        # take additional information from "Szczegóły ogłoszenia" section
-        details_main_div = bs.find("div", attrs={"id": "detailsTable"})
-        detail_elements = details_main_div.find_all("li")
-
-        # iterate over all additional information elements
-        for detail_element in detail_elements:
-            detail_title = detail_element.find("strong")
-            detail_value = detail_element.find("span")
-
-            if detail_title.text == " " and len(detail_value.text.split(":")) > 1:
-                # detail information may not have title but still contain useful information
-                # example format technika budowy: tradycyjna/cegła; okna: plastikowe
-                # It would be helpful to have technika budowy and okna variables in the dataframe
-                multiple_infor = detail_value.text.split(";")  # first we split info by semicolon sign
-                for single_inf in multiple_infor:
-                    # Iterate over elements separated by semicolon
-                    split_title, split_value = single_inf.split(":")  # split wanted title -> values pairs
-                    detail_title = unidecode(split_title)
-                    detail_value = unidecode(split_value)
-                    res[detail_title] = detail_value
+            # Check if ad contain price. Some advertisements don't contain price because they require
+            # contact with the saler
+            if primary_price_element and secondary_price_element:
+                primary_price = unidecode(primary_price_element.text)
+                secondary_price = unidecode(secondary_price_element.text)
             else:
-                # detail information are good formatted and contain span and strong elements
-                detail_title = unidecode(detail_title.text)
-                detail_value = unidecode(detail_value.text)
-                res[detail_title] = detail_value
+                # Price not included in the advertisement
+                primary_price = "Zapytaj o cene"
+                secondary_price = "Zapytaj o cene"
+            area = unidecode(area_element.text)
+            address = unidecode(address_element.text)
 
-        print(res)
-        result_list.append(res)
+            res = {
+                "URL": path,
+                "Cena": primary_price,
+                "Powierzchnia": area,
+                "Cena za metr": secondary_price,
+                "Adres": address,
+            }
+
+            # take information under the photo, like floor and year of construction
+            main_offer_div = bs.find("div", attrs={"class": "box-offer-inside mod-a mobile"})
+            main_info_div = main_offer_div.find("div", attrs={"class": "table-d__changer"})
+            main_info_divs = main_info_div.contents
+            for info_div in main_info_divs:
+                # iterate over every element with main information (under photo)
+                if info_div.name == "div":
+                    info = info_div.contents[1]
+                    title_element = info.find("p")
+                    value_elements = info.find_all("span")
+
+                    title = title_element.text
+                    title = unidecode(title)
+
+                    # value of main info can contain multiple span elements e.g. floor 3/3.
+                    # Need to iterate over all elements to create value string
+                    value = ""
+                    for value_element in value_elements:
+                        value += value_element.text
+                    value = unidecode(value)
+
+                    res[title] = value
+
+            # take additional information from "Szczegóły ogłoszenia" section
+            details_main_div = bs.find("div", attrs={"id": "detailsTable"})
+            detail_elements = details_main_div.find_all("li")
+
+            # iterate over all additional information elements
+            for detail_element in detail_elements:
+                detail_title = detail_element.find("strong")
+                detail_value = detail_element.find("span")
+
+                if detail_title.text == " " and len(detail_value.text.split(":")) > 1:
+                    # detail information may not have title but still contain useful information
+                    # example format technika budowy: tradycyjna/cegła; okna: plastikowe
+                    # It would be helpful to have technika budowy and okna variables in the dataframe
+                    multiple_infor = detail_value.text.split(";")  # first we split info by semicolon sign
+                    for single_inf in multiple_infor:
+                        # Iterate over elements separated by semicolon
+                        split_list = single_inf.split(":")  # split wanted title -> values pairs
+                        if len(split_list) == 2:
+                            detail_title = unidecode(split_list[0])
+                            detail_value = unidecode(split_list[1])
+                        else:
+                            detail_title = unidecode(split_list[0])
+                            detail_value = unidecode("")
+                        res[detail_title] = detail_value
+                else:
+                    # detail information are good formatted and contain span and strong elements
+                    detail_title = unidecode(detail_title.text)
+                    detail_value = unidecode(detail_value.text)
+                    res[detail_title] = detail_value
+
+            result_list.append(res)
+        else:
+            print(path)
 
     df = pd.DataFrame(result_list)
     df.replace(";", ",", inplace=True, regex=True)
--- main_otodom.py
+++ main_otodom.py
@@ -6,30 +6,28 @@ from selenium import webdriver
 from unidecode import unidecode
 
 
-def get_price_tag_based_on_title_tag(title_tag: str) -> Tag:
-    title_tag_siblings = list(title_tag.next_siblings)
-    for title_sibling in title_tag_siblings:
-        if title_sibling.attrs.get("aria-label") == "Cena":
-            return title_sibling
-
-
-def get_paths_from_listening_page(adds_list_url: str) -> List[str]:
-    driver.get(adds_list_url)
+def get_paths_from_listening_page(ads_list_url: str) -> List[str]:
+    """Get all url to detail ad information from the listening page"""
+    driver.get(ads_list_url)
+    # needs to scroll page down to get all site content
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     page_source = driver.page_source
     bs = BeautifulSoup(page_source, "html.parser")
 
-    search_div = bs.find_all("div", attrs={"data-cy": "search.listing"})
+    search_div = bs.find("div", attrs={"data-cy": "search.listing.organic"})
+    # check if page was loaded successfully. Sometime otodom return a dummy page with error
     if not search_div:
+        # if the invalid page was returned, reload the site
         driver.refresh()
         page_source = driver.page_source
         bs = BeautifulSoup(page_source, "html.parser")
-        search_div = bs.find_all("div", attrs={"data-cy": "search.listing"})
+        search_div = bs.find("div", attrs={"data-cy": "search.listing.organic"})
 
+    # take all urls from the listing site based on a tag in HTML li
     paths = []
     if search_div:
-        li_elements = search_div[1].findChildren("li")
+        li_elements = search_div.findChildren("li")
         for li_element in li_elements:
             a_tag = li_element.findChildren("a")
             if a_tag:
@@ -38,34 +36,64 @@ def get_paths_from_listening_page(adds_list_url: str) -> List[str]:
     return paths
 
 
-def get_title_and_price(bs) -> Tuple[Optional[str], Optional[str]]:
+def get_price_tag_based_on_title_tag(title_tag: str) -> Tag:
+    """Helper function which gets the HTML tag with the price value"""
+    title_tag_siblings = list(title_tag.next_siblings)
+    for title_sibling in title_tag_siblings:
+        if title_sibling.attrs.get("aria-label") == "Cena":
+            return title_sibling
+
+
+def get_address_tag_based_on_title_tag(title_tag: Tag) -> Tag:
+    """Helper function which gets the HTML tag with the price value"""
+    address_tag = title_tag.find("a", {"aria-label": "Adres"})
+    if not address_tag:
+        address_tag = None
+    return address_tag
+
+
+def get_title_and_price(bs: BeautifulSoup) -> Tuple[Optional[str], Optional[str]]:
+    """Get title and price information from the detail site"""
     house_title_tag = bs.find("h1", attrs={"data-cy": "adPageAdTitle"})
+
+    # check if page was loaded successfully. Sometime otodom return a dummy page with error
     if not house_title_tag:
+        # if the invalid page was returned, reload the site
         driver.refresh()
         page_source = driver.page_source
         bs = BeautifulSoup(page_source, "html.parser")
         house_title_tag = bs.find("h1", attrs={"data-cy": "adPageAdTitle"})
 
     if house_title_tag:
+        # extract the price and house title value from the website
         price_tag = get_price_tag_based_on_title_tag(house_title_tag)
+        address_tag = get_address_tag_based_on_title_tag(house_title_tag.parent)
 
         house_title = house_title_tag.contents[0]
         price_value = price_tag.contents[0]
+
         house_title = unidecode(house_title)
         price_value = unidecode(price_value)
-        return house_title, price_value
+        address_value = unidecode(address_tag.text)
+        return house_title, price_value, address_value
+
+    # If website don't contain price and house title return None in order to correctly create final dictionary
     return None, None
 
 
-def get_main_information(bs):
+def get_main_information(bs: BeautifulSoup) -> None:
+    """Get main information about the ad. Information from the 'Szczegoly ogloszenia' section """
     main_data_div = bs.find("div", attrs={"data-testid": "ad.top-information.table"})
 
+    # check if page was loaded successfully. Sometime otodom return a dummy page with error
     if not main_data_div:
+        # if the invalid page was returned, reload the site
         driver.refresh()
         page_source = driver.page_source
         bs = BeautifulSoup(page_source, "html.parser")
         main_data_div = bs.find("div", attrs={"data-testid": "ad.top-information.table"})
 
+    # every information is under the separate div element. Need to iterate over it to extract all info
     data_divs = main_data_div.findChild("div")
     for data_div in data_divs:
         _, data_name_div, data_value_div = data_div.contents
@@ -73,18 +101,21 @@ def get_main_information(bs):
         data_name = unidecode(data_name)
 
         if len(data_value_div.contents) != 1:
+            # For some information there is a div under div which contains desired value
             data_value_div = data_value_div.find("div")
 
         value = data_value_div.string
 
         if not value:
+            # For some information there is more complicated website structure to get desired value
             value = data_value_div.contents[0].contents[0].string
 
         value = unidecode(value)
         res[data_name].append(value)
 
 
-def get_additional_information(bs):
+def get_additional_information(bs: BeautifulSoup) -> None:
+    """Get additional information from the 'Informacje dodatkowe' section"""
     additional_information_main_div = bs.find("div", attrs={"data-testid": "ad.additional-information.table"})
 
     if additional_information_main_div:
@@ -114,15 +145,22 @@ def get_additional_information(bs):
         res["Material budynku"].append(None)
 
 
-def get_detail_info(detail_site_url: str):
+def get_detail_info(detail_site_url: str) -> None:
+    """Function responsible for extracting all detail information about the add from the website"""
     driver.get(detail_site_url)
     page_source = driver.page_source
     bs = BeautifulSoup(page_source, "html.parser")
-    res["Url"].append(detail_site_url)
 
-    house_title, price_value = get_title_and_price(bs)
+    try:
+        house_title, price_value, address_value = get_title_and_price(bs)
+    except:
+        print(detail_site_url)
+        house_title = None
 
     if house_title:
+        res["Url"].append(detail_site_url)
+        res["Miasto"].append(city_col)
+        res["Adres"].append(address_value)
         res["Tytul"].append(house_title)
         res["Cena"].append(price_value)
 
@@ -137,18 +175,32 @@ if __name__ == "__main__":
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--incognito')
     # options.add_argument('--headless')
-
     driver = webdriver.Chrome(chrome_options=options)
 
+    # take number of listing pages regarding specified city (first listing page)
+    listing_url = "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/warszawa"
+    file_name = "otodom/otodom_warszawa7.csv"
+    city_col = "warszawa"
+    driver.get(listing_url)
+    source_code = driver.page_source
+
+    bs = BeautifulSoup(source_code, "html.parser")
+    pagination_list = bs.find("nav", attrs={"data-cy": "pagination"})
+    pagination_list_element = pagination_list.contents[-2]
+    list_site_max_number = int(pagination_list_element.text)
+
+    # Take ad urls from the listing page
     all_paths = []
-    for i in range(1, 10):
-        url = f"https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/warszawa?page={i}"
-        listed_paths = get_paths_from_listening_page(adds_list_url=url)
+    for i in range(450, 517):
+        url = f"{listing_url}?page={i}"
+        listed_paths = get_paths_from_listening_page(ads_list_url=url)
         all_paths.extend(listed_paths)
 
+    # Get detail information about each ad
     main_url = "https://www.otodom.pl"
-
     res = {
+        "Miasto": [],
+        "Adres": [],
         "Tytul": [],
         "Url": [],
         "Cena": [],
@@ -181,4 +233,4 @@ if __name__ == "__main__":
         get_detail_info(detail_url)
 
     df = pd.DataFrame(res)
-    df.to_csv("test.csv", index=False)
+    df.to_csv(file_name, index=False, sep="\t")
