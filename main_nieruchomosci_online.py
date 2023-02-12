45bbbdf881b90488a7e861c10dac3a4fd034b4e5
Eryk <eryk658@wp.pl>
2023-02-12 09:25:30 +0100

    take appropriate address from nieruchomosci-online

--- main_nieruchomosci_online.py
+++ main_nieruchomosci_online.py
@@ -15,8 +15,9 @@ if __name__ == "__main__":
     driver = webdriver.Chrome(chrome_options=options)
 
     # take number of listing pages regarding specified city (first listing page)
-    listing_page_url = "https://warszawa.nieruchomosci-online.pl"
-    file_name = "nieruchomosci/nieruchomosci_online_warszawa6.csv"
+    city = "Gdansk"
+    listing_page_url = "https://gdansk.nieruchomosci-online.pl"
+    file_name = "nieruchomosci/nieruchomosci_online_gdansk1.csv"
 
     driver.get(listing_page_url)
     source_code = driver.page_source
@@ -29,7 +30,7 @@ if __name__ == "__main__":
 
     # Take ad urls from the listing page
     paths = []
-    for i in range(400, 500):
+    for i in range(1, 10):
         driver.get(f"{listing_page_url}/?p={i}")
         source_code = driver.page_source
         bs = BeautifulSoup(source_code, "html.parser")
@@ -44,8 +45,8 @@ if __name__ == "__main__":
                     paths.append(a_href)
 
     # paths = [
-    #     "https://warszawa.nieruchomosci-online.pl/mieszkanie-w-apartamentowcu,wysoki-standard/23877359.html",
-    #     "https://warszawa.nieruchomosci-online.pl/mieszkanie,z-aneksem-kuchennym/23787685.html"
+    #     "https://borkowo.nieruchomosci-online.pl/nowy-dom,osiedle-zielony-zakatek-ii/23527745.html",
+    #     "https://warszawa.nieruchomosci-online.pl/lokal-uzytkowy,na-sprzedaz/23880094.html"
     # ]
     result_list = []
     for path in paths:
@@ -55,12 +56,13 @@ if __name__ == "__main__":
         bs = BeautifulSoup(source_code, "html.parser")
 
         # Take the most important information about advertisement like price and area
+        print(path)
         price_main_div = bs.find("div", attrs={"id": "bottomBox"})
         if price_main_div:
             primary_price_element = price_main_div.find("p", attrs={"class": "info-primary-price"})
             area_element = price_main_div.find("p", attrs={"class": "info-area"})
             secondary_price_element = price_main_div.find("p", attrs={"class": "info-secondary-price"})
-            address_element = price_main_div.find("h1")
+            title_element = price_main_div.find("h1")
 
             # Check if ad contain price. Some advertisements don't contain price because they require
             # contact with the saler
@@ -72,14 +74,29 @@ if __name__ == "__main__":
                 primary_price = "Zapytaj o cene"
                 secondary_price = "Zapytaj o cene"
             area = unidecode(area_element.text)
-            address = unidecode(address_element.text)
+            title = unidecode(title_element.text)
 
+            # take full advertisement address
+            address_div = bs.find("div", attrs={"id": "locationTable"})
+            list_element_with_address = address_div.find("li", attrs={"class": "adress"})
+            if list_element_with_address:
+                span_address = list_element_with_address.find("span")
+                address = unidecode(span_address.text)
+            else:
+                list_elements = address_div.find_all("li")
+                for list_element in list_elements:
+                    element_text = list_element.text
+                    split_text = element_text.split(":")
+                    if len(split_text) == 2 and split_text[0] == "Adres":
+                        address = unidecode(split_text[1])
             res = {
+                "Miasto": city,
                 "URL": path,
                 "Cena": primary_price,
                 "Powierzchnia": area,
                 "Cena za metr": secondary_price,
-                "Adres": address,
+                "Tytul": title,
+                "Adres": address
             }
 
             # take information under the photo, like floor and year of construction
