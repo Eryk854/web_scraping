c328e2d5c536d542e2f9064b5447426585ddb7e9
Eryk <eryk658@wp.pl>
2023-02-28 21:04:31 +0100

    add nieruchomosci online initial preprocessing

--- initial_preprocess.py
+++ initial_preprocess.py
@@ -4,18 +4,29 @@ import pandas as pd
 file_path = "otodom/otodom.csv"
 df = pd.read_csv(file_path, sep="\t")
 df = df[(~df["Cena"].isin(["1 000 000 USD", "1 000 000 EUR"])) & (df["Cena"] != "Zapytaj o cene")]
-df.to_csv("otodom/otodom_with_price.csv", index=False, sep="\t")
+df.to_csv("otodom/otodom_initial_preprocess.csv", index=False, sep="\t")
 
 # initial preparation of nieruchomosci-online data
 file_path = "nieruchomosci/nieruchomosci_online.csv"
 df = pd.read_csv(file_path, sep="\t")
 types_of_advert = [
     "mieszkanie na sprzedaz",
-    "maly dom na sprzedaz",
     "nowe mieszkanie na sprzedaz",
-    "nowy dom na sprzedaz",
-    "dom na sprzedaz",
     "kawalerka na sprzedaz"
 ]
-df = df.loc[(df["Typ oferty:"].isin(types_of_advert)) & (df["Cena"] != "Zapytaj o cene")]
-df.to_csv("nieruchomosci/nieruchomosci_online_with_price.csv", index=False, sep="\t")
+df = df.loc[
+    (df["Typ oferty:"].isin(types_of_advert)) &
+    (df["Cena"] != "Zapytaj o cene") &
+    (~df["Cena"].str.contains("EUR")) &
+    (~df["Cena"].str.contains("\$"))
+]
+df = df.loc[:, ~df.isna().all()]
+
+df.loc[(df[" okna"].isna()), " okna"] = ""
+df.loc[(df["okna"].isna()), "okna"] = ""
+df["okna"] = df[" okna"] + df["okna"]
+df = df.drop(columns=["Mozliwa zamiana:", "Stan inwestycji:", "Mozliwe przeznaczenie:", "Cena:", "Rozklad mieszkania:", "  ", " okna"])
+df = df.rename(columns={" ": "winda"})
+new_columns_names = [column_name.replace(":", "") for column_name in df.columns]
+df.columns = new_columns_names
+df.to_csv("nieruchomosci/nieruchomosci_online_initial_preprocess.csv", index=False, sep="\t")
